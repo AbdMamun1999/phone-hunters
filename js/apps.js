caa639f74b438e19cf602d3eb7e3561955fcf2c2
AbdMamun1999 <mamunrox@gmail.com>
2022-03-01 10:29:15 +0600

    add a function for display phone

--- js/apps.js
+++ js/apps.js
@@ -10,6 +10,30 @@ const loadPhone = searchText =>{
     console.log(url)
     fetch(url)
         .then(response => response.json())
-        .then(data => console.log(data.data))
+        .then(data => displayData(data.data))
+}
+
+const displayData = phones =>{
+    console.log(phones)
+    const displayCardDiv = document.getElementById('display-card')
+    console.log(displayCardDiv)
+    phones.forEach(phone => {
+        console.log(phone)
+        const div = document.createElement('div');
+        div.classList.add('flex')
+        div.innerHTML = `
+            <div class="">
+                <img src="${phone.image}" alt="" class="h-full">
+            </div>
+            <div class="">
+                <h1 class="text-xl py-3">${phone.phone_name}</h1>
+                <h3 class="mb-3">${phone.brand}</h3>
+                <div>
+                    <button class="bg-yellow-400 px-4 py-2 rounded text-white">Details</button>
+                </div>
+            </div>
+        `;
+        displayCardDiv.appendChild(div)
+    });
 }
 
