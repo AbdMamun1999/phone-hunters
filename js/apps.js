e9f183ded00228179e2717a91fa05994fb154d9b
AbdMamun1999 <mamunrox@gmail.com>
2022-03-01 08:41:27 +0600

    add a function for get data from api search by name

--- js/apps.js
+++ js/apps.js
@@ -1,6 +1,15 @@
 // Get value from input field
 const searchPhone = () =>{
     const searchText = document.getElementById('input-field').value
-    console.log(searchText)
+    loadPhone(searchText)
+}
+
+// get data from api search by name
+const loadPhone = searchText =>{
+    const url =  `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    console.log(url)
+    fetch(url)
+        .then(response => response.json())
+        .then(data => console.log(data.data))
 }
 
